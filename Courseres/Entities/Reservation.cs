6cc45aec37c8761d9da36624db4f58976b8e499a
Jose Marcus <onlymarcus@hotmail.com>
2019-05-24 13:58:19 -0300

    Commit inicial aula Try Catch

---
+++ Courseres/Entities/Reservation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Courseres.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", Chek-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", Chek-Out: "
+                + CheckOut.ToString("dd/MM/yyy")
+                + ", "
+                + Duration()
+                + " nights.";
+        }
+    }
+}
